9bd03a823a8ce9d91042aeeb42516e3c10c6cb7b
Atharva Dandekar <dandekar.atharva@gmail.com>
2017-02-22 19:23:33 +0530

    Changed detailViewController UI.

--- SampleTableViewApp/detailViewController.swift
+++ SampleTableViewApp/detailViewController.swift
@@ -28,6 +28,11 @@ class detailViewController: UIViewController {
     var movieId: Int = -1
     var imdbId: String = ""
     
+    func roundIt(value: Float, step: Float) -> Float {
+        let inv = 1.0 / step
+        return Float(round(value * inv) / inv)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.loader.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
@@ -46,16 +51,46 @@ class detailViewController: UIViewController {
         
         myScrollView.contentSize = CGSize(width: self.view.frame.size.width, height: 650)
         
-        Alamofire.request("https://api.themoviedb.org/3/movie/\(self.movieId)?api_key=01082f35da875726ce81a65b79c1d08c").responseJSON { response in
+        Alamofire.request("https://api.themoviedb.org/3/movie/\(self.movieId)?api_key=01082f35da875726ce81a65b79c1d08c&append_to_response=reviews,credits,videos,images").responseJSON { response in
             
             if let jsonValue = response.result.value {
                 let results = JSON(jsonValue)
                 self.imdbId = results["imdb_id"].stringValue
                 self.movieReleasedLabel.text = "Released on " + results["release_date"].stringValue
-                self.movieLanguageLabel.text = "Language - " + results["original_language"].stringValue
-                self.movieVotesLabel.text = "Vote average - " + results["vote_average"].stringValue
-                self.movieRuntimeLabel.text = "Runtime - " + results["runtime"].stringValue
-                self.movieOverviewTextView.text = results["overview"].stringValue
+                self.movieLanguageLabel.text = results["original_language"].stringValue.uppercased()
+                self.movieRuntimeLabel.text = results["runtime"].stringValue + " mins"
+                self.movieOverviewTextView.text = results["overview"].stringValue                
+                
+                //Iterate ratings
+                let starFilled = UIImage(named: "star_filled")
+                let starHalf = UIImage(named: "star_half")
+                let rating = self.roundIt(value: results["vote_average"].floatValue/2, step: 0.5)
+                let incrementer = 20
+                let start = 265
+                
+                if rating.truncatingRemainder(dividingBy: 1) == 0 {
+                    let ratingInt = Int(rating)
+                    for i in 0..<ratingInt {
+                        let imageView: UIImageView = UIImageView(image: starFilled)
+                        imageView.frame = CGRect(x: start + (i * incrementer), y: -23, width: 20, height: 20)
+                        self.myScrollView.addSubview(imageView)
+                    }
+                }
+                else {
+                    let rnd: Int = Int(rating - 0.5)
+                    var lastPlace: Int = 0
+                    for i in 0..<rnd {
+                        let imageView: UIImageView = UIImageView(image: starFilled)
+                        imageView.frame = CGRect(x: start + (i * incrementer), y: -23, width: 20, height: 20)
+                        lastPlace = start + (i * incrementer)
+                        self.myScrollView.addSubview(imageView)
+                    }
+                    let imageViewHalf: UIImageView = UIImageView(image: starHalf)
+                    imageViewHalf.frame = CGRect(x: lastPlace + incrementer, y: -23, width: 20, height: 20)
+                    self.myScrollView.addSubview(imageViewHalf)
+                }
+                
+                //backdrop image
                 if results["backdrop_path"] != JSON.null {
                     let imageUrl: String = results["backdrop_path"].stringValue
                     let url = URL(string: "https://image.tmdb.org/t/p/w500/" + imageUrl)
